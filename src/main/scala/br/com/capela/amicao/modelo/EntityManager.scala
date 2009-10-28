d12381dd03fa19ca74d65eceb4526e4765a8a51f
renatos <renato.otaner@gmail.com>
2009-10-28 09:28:08 -0200

    movendo arquivos

--- src/main/scala/br/com/capela/amicao/modelo/EntityManager.scala
+++
@@ -1,9 +0,0 @@
-package br.com.capela.amicao.modelo;
-
-import javax.persistence._
-
-object EntityManager {
-	private var em:EntityManager = Persistence createEntityManagerFactory "TesteAmicao" createEntityManager;
-	
-	def get(): EntityManager = em
-}
