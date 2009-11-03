b12aa075f72f689042230080c76eb9958c8af244
renatos <renato.otaner@gmail.com>
2009-11-02 23:21:40 -0200

    melhorando a camada de persistencia

--- src/main/scala/br/com/capela/persistencia/EntityManager.scala
+++ src/main/scala/br/com/capela/persistencia/EntityManager.scala
@@ -1,9 +1,55 @@
 package br.com.capela.persistencia;
 
+import br.com.capela.amicao.modelo.base.Entidade
+
 import javax.persistence._
 
 object EntityManager {
 	private var em:EntityManager = Persistence createEntityManagerFactory "TesteAmicao" createEntityManager;
 	
 	def apply(): EntityManager = em
+	
+	def salvar(entidade:Entidade[_]):Unit = {
+		if (entidade.id < 0)
+			em persist(entidade) 
+		else 
+			em merge(entidade)
+	}
+	
+	def excluir(entidade:Entidade[_]) {
+		em remove(entidade)
+	}
+	
+	def find(clazz:Class[_], id:Int):Entidade[_] = {
+		var entidade = find(clazz,id)
+		entidade.asInstanceOf[Entidade[_]]
+	}
+	
+	def findAll(clazz:Class[_]):java.util.List[Entidade[_]] = {
+		implicit def toQueryString(clazz:Class[_]):String = {String.format("select e from %s e order by e.id", clazz.getSimpleName())}
+		
+		var entidades:java.util.List[_] = em createQuery(clazz) getResultList()
+		entidades.asInstanceOf[java.util.List[Entidade[_]]]
+	}
+	
+	def findByQuery(umaConsulta:Query):java.util.List[Entidade[_]] = {
+		var entidades:java.util.List[_] = em createQuery(umaConsulta) getResultList()
+		entidades.asInstanceOf[java.util.List[Entidade[_]]]
+	}
+}
+
+class ExecutarEmTransacao(et:EntityTransaction){
+	def usando(codeBlock: (EntityManager, Entidade[_]) => Unit): Unit = {
+		et begin()
+		try{
+			codeBlock(em,entidade)
+			em flush()
+			et commit()	
+		}
+		catch {
+			case ex : Exception => et rollback
+		}
+	}	
+	
+	
 }
--- src/test/scala/br/com/capela/amicao/modelo/pet/PetSpec.scala
+++ src/test/scala/br/com/capela/amicao/modelo/pet/PetSpec.scala
@@ -2,8 +2,8 @@ package br.com.capela.amicao.modelo.pet
 
 import org.scalatest.Spec;
 import org.scalatest.BeforeAndAfter;
-import org.scalatest.matchers._;
-import org.scalatest.verb._;
+import org.scalatest.matchers.MustMatchers;
+import org.scalatest.verb.Spec;
 
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
@@ -12,21 +12,22 @@ import org.junit._
 import br.com.capela.amicao.modelo.pet._
 import br.com.capela.amicao.modelo.cliente._
 import br.com.capela.persistencia._
-import br.com.capela.persistencia.cliente._
+//import br.com.capela.persistencia.cliente._
 import br.com.capela.amicao.modelo.base.Entidade
 
 
 @RunWith(classOf[JUnitRunner])
 class PetSpec extends Spec with MustMatchers{
-	
+
 	describe("Pet") {
 		it("deve ser persistida"){
 			var pet:Pet = new Pet();
 			pet salvar() ;
 			pet.id must be > 0
-			ClienteRepository.listarTodos()
+			//ClienteRepository.listarTodos()
 			pet nome = "Teste"
 			pet salvar
 		}
 	}
 }
+
