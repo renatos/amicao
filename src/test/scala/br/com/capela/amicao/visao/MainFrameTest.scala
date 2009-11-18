5b230c931feb2f0a50fbf2b642a9f3d3d6d7b74d
renatos <renato.otaner@gmail.com>
2009-11-18 00:13:03 -0200

    Testes para a interface gráfica

--- src/test/scala/br/com/capela/amicao/visao/MainFrameTest.scala
+++ src/test/scala/br/com/capela/amicao/visao/MainFrameTest.scala
@@ -4,7 +4,7 @@ import java.awt.Dimension
 import swing._
 import swing.event._
 import GridBagPanel._
-import javax.swing.{JInternalFrame,JDesktopPane}
+import javax.swing.{JInternalFrame,JDesktopPane,JPanel}
 
 import br.com.capela.amicao.servico.cliente.ClienteService
 import br.com.capela.amicao.servico.pet.PetService
@@ -24,29 +24,30 @@ import org.springframework.beans.factory.annotation.Autowired
 object MainFrameTest extends SimpleGUIApplication{
 	var desktop = new DesktopPane 
 	val ui = new BoxPanel(Orientation.Vertical) {
-		
-		
 		contents += desktop
-		
 	}
  	
  	def createInternalFrame() = {
- 		var internalFrame = new JInternalFrame("Frame Interno",true, true, true, true)
+ 		var internalFrame = new InternalFrame("Frame Interno",true, true, true, true)
 		internalFrame.pack
-		internalFrame.setVisible(true)
-		internalFrame.setSize(100,100)
+		internalFrame.visible = true
+		internalFrame.size=(100,100)
 		desktop.add(internalFrame)
  	}
  	
+ 	def createRacaForm() = {
+ 	  var racaForm = new JPanel()
+ 	  racaForm
+ 	}
+ 	
+  
 	def top = new MainFrame {
-		title = "Amicão"
+		title = "Amicao"
 		menuBar = new MenuBar 
 		var menu = new Menu("Teste")
 		
 		menu.contents += new MenuItem("Item");
-		menu.contents += new MenuItem(Action("An action item") {
-		      createInternalFrame
-		    });
+		menu.contents += new MenuItem(Action("An action item") {createInternalFrame});
 		menuBar.contents += menu
 		preferredSize=(600,400)
 		contents = ui
@@ -55,11 +56,14 @@ object MainFrameTest extends SimpleGUIApplication{
 
 class DesktopPane extends Component {
 	override lazy val peer: JDesktopPane = new JDesktopPane() with SuperMixin
-	
 	def add(c:java.awt.Component) = peer.add(c)
 }
-class InternalFrame(nome:String, p1:Boolean,p2:Boolean,p3:Boolean) extends Component {
-	override lazy val peer: JInternalFrame = new JInternalFrame(nome,p1,p2,p3) with SuperMixin
+class InternalFrame(nome:String, p1:Boolean,p2:Boolean,p3:Boolean,p4:Boolean) extends Component {
+	override lazy val peer: JInternalFrame = new JInternalFrame(nome,p1,p2,p3,p4) with SuperMixin
+	
+	def pack() = peer.pack
+	def visible_=(visible:Boolean) = peer.setVisible(visible)
+	def size_=(xy: (Int, Int)) { peer.setPreferredSize(new Dimension(xy._1, xy._2)) }
 }
 
 
